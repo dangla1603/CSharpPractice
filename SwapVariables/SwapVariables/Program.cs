06c2a10b42fb280c09d0c87386c83a05dd8c9119
dang la <dangla1603@gmail.com>
2022-04-17 17:26:00 -0500

    swapping variables

---
+++ SwapVariables/SwapVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwapVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // To be able to swap variables
+            // using a temporary variable 
+            /*define swap(x,y)
+                temp:= x
+                x:=y
+                y:=temp*/
+
+            int num1, num2, tempNum;
+
+            Console.Write("\nInput first number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInput second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            tempNum = num1;
+            num1 = num2;
+            num2 = tempNum;
+
+            //Printing result after swapping
+            Console.Write("\nResult: ");
+            Console.Write("\nfirst Number: " + num1);
+            Console.Write("\nsecond Number: " + num2);
+        }
+    }
+}
