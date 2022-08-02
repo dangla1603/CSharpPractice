424948229b87a5c7d2adc194667230a600595c68
dang la <dangla1603@gmail.com>
2022-08-02 15:46:32 -0500

    add some mini games

--- RockPaperScissorsGame/RockPaperScissorsGame/Program.cs
+++ RockPaperScissorsGame/RockPaperScissorsGame/Program.cs
@@ -63,7 +63,7 @@ namespace RockPaperScissorsGame
                 else
                 {
                     Console.WriteLine("You Lose");
-                }              
+                }                 
             }
 
         
